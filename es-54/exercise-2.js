7c7bb0e37f6152f3fb72a4c3a5d53499bc3aabc3
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-05-14 22:28:34 +0200

    fatto

---
+++ es-54/exercise-2.js
@@ -0,0 +1,21 @@
+function nicknameMap(persons) { 
+  // per far creare un nuovo array facciamo il return di persons.map seguito da un arraw functions con dentro persons e gli items del nuovo array
+  return persons.map((persons) => `${persons.name}-${persons.age}`)
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
