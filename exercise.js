6b5e130f51f2f311fe9fd8cbb928a67883e2ba74
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-06-02 16:57:51 +0200

    done

---
+++ exercise.js
@@ -0,0 +1,39 @@
+const isLogged = true;
+
+
+// function chaining  (isLogged) {
+//     return new Promise((resolve, reject)) => {
+//         if (isLogged == true){ 
+//             resolve(Math.random()) 
+//     }}
+// }
+
+
+//**! creiamo la prima funzione che ci ritornerà una nuova promise dove se is logged è true genererà un numero random altrimente si bloccherà e dirà false */
+function chaining(isLogged) {
+    return new Promise((resolve, reject) => {
+        if (isLogged == true){
+            resolve(Math.random())
+        } else {
+            reject(new Error("isLogged is false"))
+        }
+    })
+}
+
+//**! creiamo un'altra funzione dove se il prodotto della funzione precendete è maggiore di 0.5 */
+
+
+function promiseChaining(chaining) {
+    return new Promise((resolve, reject) => {
+        if (chaining > 0.5) {
+            resolve(`{name: "John", age: 24}`)
+        } else {
+            reject(new Error("chaining is minor than 0.5"))
+        }
+    })
+}
+
+chaining(isLogged)
+.then(promiseChaining)
+.then((isLogged) => console.log("l'utente è online"))
+.catch((error) => console.error("c'è qualcosa che non va"))
