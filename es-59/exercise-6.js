3cb3d76fbec4f9260b9c7192bd26a8cd5c432149
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-06-04 17:42:54 +0200

    done

---
+++ es-59/exercise-6.js
@@ -0,0 +1,38 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+  static calculate = (shapes) => {
+    if (shapes instanceof Square){
+      return shapes.side **2
+    } else if (shapes instanceof Rectangle){
+      return shapes.width * shapes.height
+  } else if (shapes instanceof Circle){
+    return shapes.radius **2 *Math.PI
+    }
+  }
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
