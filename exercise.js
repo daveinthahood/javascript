faef80eca9bb92b34758e768a641c498876912c9
Dave <101344486+daveinthahood@users.noreply.github.com>
2023-06-05 08:24:35 +0200

    done

---
+++ exercise.js
@@ -0,0 +1,24 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  toJson() {
+    return JSON.stringify(this);
+  }
+
+
+static fromJson(json) {
+    const obj = JSON.parse(json);
+    const { id, firstName, lastName, age } = obj;
+    return new Person(id, firstName, lastName, age);
+  }
+}
+
+
+const json = '{"id":1,"firstName":"Mario","lastName":"Rossi","age":25}';
+const developer = Person.fromJson(json);
+console.log(developer);
